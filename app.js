40926d9a4b94ed7a084711c09f804cc5d059f095
localleon <me@lrau.xyz>
2022-08-16 21:36:24 +0200

    Finished first design with animation

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("UrbanSportScanner")
+
+let minutes = 0
+let seconds = 0
+
+
+function CheckInCounter(){
+    seconds += 1
+    if (seconds == 60){
+        minutes += 1
+        seconds = 0
+    }
+
+    let timestring =  "Eingecheckt: " + pad2(minutes) + ":" + pad2(seconds) + " minutes ago"
+    
+    var counter = document.getElementById("checkin-counter")
+    counter.innerHTML = timestring
+
+}
+
+function pad2(number) {
+    return (number < 10 ? '0' : '') + number
+}
+
+
+console.log("UrbanSportScanner ready")
+var cancel = setInterval(CheckInCounter,1000)
